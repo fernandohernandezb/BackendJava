c47b16443f5abdc8747c468bffed6da6e5632273
fernandohernandezb <43483318+fernandohernandezb@users.noreply.github.com>
2022-05-02 23:22:31 -0500

    Delete

--- practicaMultiverse/src/CallBacksGwenStacy.java
+++
@@ -1,6 +0,0 @@
-public interface CallBacksGwenStacy {
-    void fuerzaSobrehumana(Screen s);
-    void velocidadSobrehumana(Screen s);
-    void reflejosSobrehumana(Screen s);
-    void sentidoAracnido(Screen s);
-}
--- practicaMultiverse/src/CallBacksPeterParker.java
+++
@@ -1,7 +0,0 @@
-public interface CallBacksPeterParker {
-    //Lo que hace
-    void fuerzaSobrehumana(Screen s);
-    void curacion(Screen s);
-    void reflejosSobrehumana(Screen s);
-    void trepaMuros(Screen s);
-}
